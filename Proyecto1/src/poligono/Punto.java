c7aab8f96079f5b32f694006fa4e7c2b7d9a7541
sarki <sarki@172.20.46.158>
2016-03-14 19:28:28 +0100

    Creacion clases punto y vector

--- Proyecto1/src/poligono/Punto.java
+++ Proyecto1/src/poligono/Punto.java
@@ -9,4 +9,22 @@ public class Punto {
 		this.posX=posX;
 		this.posY=posY;
 	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	
+	
 }
---
+++ Proyecto1/src/poligono/Vector.java
@@ -0,0 +1,20 @@
+package poligono;
+
+public class Vector {
+
+	Punto punto1;
+	Punto punto2;
+	
+	
+	public Vector(Punto punto1,Punto punto2){
+		this.punto1=punto1;
+		this.punto2=punto2;
+	}
+	
+	public int productoVectorial(Vector vector1,Vector vector2){
+		
+		
+		
+		return 0;
+	}
+}
